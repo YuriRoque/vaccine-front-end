49669f48f89a4a21cd5ceefb0453167445362182
Yuri Roque Benvenutti <yrbenvenutti@gmail.com>
2022-04-24 22:24:54 -0400

    Configured the routes to home and scheduler page

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Scheduler from './pages/Scheduler';
 
 const Router = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/'></Route>
+          <Route path='/' element={<Layout />}>
+            <Route element={<Home />} index />
+            <Route path='scheduler' element={<Scheduler />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
